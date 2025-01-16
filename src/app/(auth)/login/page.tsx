4f8c69581570f9c683568cceb4064863924b17e9
Nikita <ragerage333@gmail.com>
2025-01-16 13:25:44 +0300

    pre db

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import LoginForm from "./LoginForm";
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center vertical-center">
-      {/* <LoginForm /> */}
+      <LoginForm />
     </div>
   );
 }
---
+++ src/auth.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import authConfig from "./auth.config";
+
+const prisma = new PrismaClient();
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  //   providers: [],
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
